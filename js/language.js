b94a29e035e2ea53c854611ba55a8e9037b6646a
progdidi <90276229+progdidi@users.noreply.github.com>
2023-06-23 16:45:30 +0200

    Update language.js

--- js/language.js
+++ js/language.js
@@ -20,8 +20,40 @@ const languageText = {
      "eng": 'I am starting frontend-developer and i will be glad to help you to make great website '
    },
    "about": {
-      "ru": 'Я закончила БГЭУ (Белорусский государственный экономический университет) и проработала бухгалтером 8 лет, но всегда чувствовала, насколько мне интересны технологии и особенно в сфере веб-разработки, я решила изменить свою жизнь. И вот уже год изучаю технику и получаю от этого огромное удовольствие! Люблю творить и постоянно развиваться!',
-      "eng": 'I graduated from the BSEU (Belarusian State Economic University) and worked as an accountant for 8 years, but I always felt how interested I was in technology and especially in the field of web development, I decided to change my life. And now I have been studying technology for a year and I get great pleasure from it! I like to create and constatntly develop! '
-    },
+   "ru": 'Я закончила БГЭУ (Белорусский государственный экономический университет) и проработала бухгалтером 8 лет, но всегда чувствовала, насколько мне интересны технологии и особенно в сфере веб-разработки, я решила изменить свою жизнь. И вот уже год изучаю технику и получаю от этого огромное удовольствие! Люблю творить и постоянно развиваться!',
+   "eng": 'I graduated from the BSEU (Belarusian State Economic University) and worked as an accountant for 8 years, but I always felt how interested I was in technology and especially in the field of web development, I decided to change my life. And now I have been studying technology for a year and I get great pleasure from it! I like to create and constatntly develop! '
+   },
+
+   "coffeshop": {
+      "ru": 'Адаптивный одностраничный сайт для магазина кофе, сделанный с использованием HTML, SCSS, Javascript. Собран через Gulp',
+      "eng": 'Responsive landing page for a coffee shop, made using HTML, SCSS, Javascript. Compiled via Gulp '
+   },
+
+   "gentlekiss": {
+      "ru": 'Адаптивный одностраничный сайт для копирайтера сделанный с использованием HTML, SCSS, Javascript. Собран через Gulp',
+      "eng": 'Responsive landing page for a copywrighter, made using HTML, SCSS, Javascript. Compiled via Gulp '
+   },
+
+   "meditation": {
+      "ru": 'Адаптивный одностраничный сайт для сайта о медитациях сделанный с использованием HTML, SCSS, Javascript. Собран через Gulp',
+      "eng": 'Responsive landing page about meditation, made using HTML, SCSS, Javascript. Compiled via Gulp '
+   },
+
+   "organik": {
+      "ru": 'Многостраничный сайт для магазина органических продуктов. Сделан с помощью React',
+      "eng": 'React manypage website for shop of organik projects'
+   },
+
+   "portfolio": {
+      "ru": 'Адаптивный многоязычный сайт-портфолио , сделанный с использованием HTML, SCSS, Javascript. Собран через Gulp',
+      "eng": 'Responsive multilanguage portfolio landing page , made using HTML, SCSS, Javascript. Compiled via Gulp'
+   },
+
+   "visit": {
+      "ru": 'перейти на сайт',
+      "eng": 'visit'
+   }
+
+
 }
 
